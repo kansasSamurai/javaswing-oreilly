9e859c2b63bff782a76af15aeac7d3516059fef5
Rick Wellman <kansasSamurai@users.noreply.github.com>
2022-10-30 12:27:07 -0500

    Update BookCellRenderer.java
    Extend DefaultListCellRenderer instead of JLabel

--- jswing2/ch07/BookCellRenderer.java
+++ jswing2/ch07/BookCellRenderer.java
@@ -1,15 +1,38 @@
-// BookCellRenderer.java
-// A custom cell renderer for BookEntry objects.
-//
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JList;
+import javax.swing.border.Border;
+
 import java.awt.*;
 
-public class BookCellRenderer extends JLabel implements ListCellRenderer {
-    private static final Color HIGHLIGHT_COLOR = new Color(0, 0, 128);
+/**
+ * BookCellRenderer.java
+ * A custom cell renderer for BookEntry objects.
+ * 
+ * This originally extends JLabel... I think it might
+ * be better to extend DefaultListCellRenderer (which
+ * also extends JLabel but has certain overloaded
+ * methods to improve performance)
+ *  
+ * extends JLabel implements ListCellRenderer 
+ *  
+ * @author Rick Wellman
+ *
+ */
+@SuppressWarnings({ "rawtypes", "serial" })
+public class BookCellRenderer extends DefaultListCellRenderer {
+	
+    private static final Color HIGHLIGHT_COLOR = new Color(0x7395AE); // new Color(0, 0, 128); // modified
 
+    private static final Border BORDER = BorderFactory.createEmptyBorder(4,4,4,4);
+    
     public BookCellRenderer() {
         setOpaque(true);
         setIconTextGap(12);
+        
+        //Have to move this line now that we are extending DefaultListCellRenderer
+        //because it overrides our setting in its implementation of getListCellRendererComponent()
+        //setBorder(BORDER); // new
     }
 
     public Component getListCellRendererComponent(
@@ -19,16 +42,23 @@ public class BookCellRenderer extends JLabel implements ListCellRenderer {
         boolean isSelected,
         boolean cellHasFocus)
     {
-        BookEntry entry = (BookEntry)value;
+    	super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+    	
+        final BookEntry entry = (BookEntry)value;
         setText(entry.getTitle());
         setIcon(entry.getImage());
-        if(isSelected) {
+        
+        setBorder(BORDER); // new
+        
+        if (isSelected) {
             setBackground(HIGHLIGHT_COLOR);
             setForeground(Color.white);
         } else {
             setBackground(Color.white);
             setForeground(Color.black);
         }
+        
         return this;
     }
+    
 }
